eac2128ad09206c994449e87f0ed0b2e9b0d6953
Eswarg7788 <eswaran161204@gmail.com>
2024-08-29 18:07:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
-function App() {
+import logo from './logo.png';
+function Student () {
+    const [regularStudents, setRegularStudents] = useState(1);
+    const [remedialStudents, setRemedialStudents] = useState(2);
+    const [paidClubStudents, setPaidClubStudents] = useState(3);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='dashboard-container'>
+    
+    <div class="dashboard">
+      <img src={logo} className='hicet'></img>
+     <a href=""><div  className='vertical'>Student Management</div></a> 
+     <a href=""> <div  className='vertical'>Financial Management</div></a>
+     <a href=""><div  className='vertical'>Quality Control</div></a> 
+     <a href=""> <div  className='vertical'>Report Delivery</div></a>
+     <a href=""> <div  className='vertical'>Attendance</div></a>
+</div>
+<div className='content'>
+      <h2>Student Management</h2></div>
+      <div className='ee'>
+      <div style={{ display: 'flex', justifyContent: 'space-around', }}>
+      <div style={{marginLeft:'300px', marginBottom:'400px', backgroundColor: 'red', padding: '60px', borderRadius: '10px', color: 'white' }}>
+        <div style={{ fontSize: '2em' }}>{regularStudents}</div>
+        <div>Regular Students</div>
+      </div>
+      <div style={{marginLeft:'20px', marginBottom:'400px', backgroundColor: 'purple', padding: '60px', borderRadius: '10px', color: 'white' }}>
+        <div style={{ fontSize: '2em' }}>{remedialStudents}</div>
+        <div>Remedial Students</div>
+      </div>
+      <div style={{marginLeft:'20px', marginBottom:'400px', backgroundColor: 'green', padding: '60px', borderRadius: '10px', color: 'white' }}>
+        <div style={{ fontSize: '2em' }}>{paidClubStudents}</div>
+        <div>In Paid Clubs</div>
+      </div>
+      
+      </div>
     </div>
+   </div>
   );
 }
 
-export default App;
+export default Student;
